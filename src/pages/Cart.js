548027fb8d3a8a289f16ddd59b05c56de22c57e9
KamrujjamanRony <kamrujjaman0228@gmail.com>
2022-12-16 01:12:32 +0600

    create store and reducers

---
+++ src/pages/Cart.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ProductCard from "../components/ProductCard";
+
+const Cart = () => {
+  // const {
+  //   state: { cart, loading, error },
+  // } = useProducts();
+
+  let content;
+
+  // if (loading) {
+  //   content = <p>Loading...</p>;
+  // }
+  // if (error) {
+  //   content = <p>Something went wrong..</p>;
+  // }
+  // if (!loading && !error && cart.length === 0) {
+  //   content = <p>There is no products. Product list is empty.</p>;
+  // }
+  // if (!loading && !error && cart.length) {
+  //   content = cart.map((product) => (
+  //     <ProductCard key={product.id} product={product}></ProductCard>
+  //   ));
+  // }
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+      {content}
+    </div>
+  );
+};
+
+export default Cart;
