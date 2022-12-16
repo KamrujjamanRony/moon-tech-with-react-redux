797be933b04c02f139ddb2a5c7dd8bcb0c6d4c3d
KamrujjamanRony <kamrujjaman0228@gmail.com>
2022-12-16 18:57:32 +0600

    display cart products to UI done

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,31 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
 
 const Cart = () => {
-  // const {
-  //   state: { cart, loading, error },
-  // } = useProducts();
-
-  let content;
-
-  // if (loading) {
-  //   content = <p>Loading...</p>;
-  // }
-  // if (error) {
-  //   content = <p>Something went wrong..</p>;
-  // }
-  // if (!loading && !error && cart.length === 0) {
-  //   content = <p>There is no products. Product list is empty.</p>;
-  // }
-  // if (!loading && !error && cart.length) {
-  //   content = cart.map((product) => (
-  //     <ProductCard key={product.id} product={product}></ProductCard>
-  //   ));
-  // }
+  const cart = useSelector((state) => state.cart);
+  
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      {content}
+    {
+      cart.map(product => <ProductCard 
+        key={product.id}
+        product={product} />)
+    }
     </div>
   );
 };
